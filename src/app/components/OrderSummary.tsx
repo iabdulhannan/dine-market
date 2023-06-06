d13925f792a142f9de73710a7481cbbd2ceb8ad7
Abdul Hannan <abdul.hannan@propsure.com.pk>
2023-06-06 10:24:51 +0500

    Improved Cart and Developed Order Summary

--- src/app/components/OrderSummary.tsx
+++ src/app/components/OrderSummary.tsx
@@ -12,27 +12,27 @@ function OrderSummary() {
   const totalCost = useAppSelector(state => state.cartReducer.products.reduce((total: number, product: ProductInCart) => total + product.price, 0))
 
   return (
-    <div className={'col-span-12 md:col-span-3'}>
-      <div className={'flex flex-col gap-y-3 p-5 rounded-lg bg-gray-50 w-full'}>
-        <TypographyH2 text={'Order Summary'} className={'text-xl text-center'}/>
-        <div className={'flex flex-row items-center justify-between gap-x-3'}>
-          <h2 className={'text-xl text-center'}>
+    <div className={'col-span-12 md:col-span-3 flex justify-center'}>
+      <div className={'flex flex-col gap-y-3 p-5 rounded-lg bg-gray-50 items-center w-full sm:w-1/2 lg:w-full'}>
+        <TypographyH2 text={'Order Summary'} className={'text-lg lg:text-xl text-center'}/>
+        <div className={'flex flex-row items-center justify-between gap-x-3 w-full'}>
+          <h2 className={'text-md lg:text-lg text-center'}>
             Product/s
           </h2>
-          <h2 className={'text-xl text-center'}>
+          <h2 className={'text-md lg:text-lg text-center'}>
             {totalProducts}
           </h2>
         </div>
-        <div className={'flex flex-row items-center justify-between gap-x-3'}>
-          <h2 className={'text-xl text-center'}>
+        <div className={'flex flex-row items-center justify-between gap-x-3 w-full'}>
+          <h2 className={'text-md lg:text-lg text-center'}>
             Cost
           </h2>
-          <h2 className={'text-xl text-center'}>
+          <h2 className={'text-md lg:text-lg text-center'}>
             ${totalCost}
           </h2>
         </div>
         <Link href={'pages/shop/checkout'}
-              className={'w-full text-white text-center font-semibold bg-black px-5 py-2 text-sm border-t-2 border-l-2 border-gray-500'}>
+              className={'w-full sm:w-1/2 lg:w-full text-white text-center font-semibold bg-black px-5 py-2 text-sm border-t-2 border-l-2 border-gray-500'}>
           <label>
             Checkout
           </label>
