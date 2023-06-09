148321fa2dc867091e4a1838e89f92ceff2cfaf0
Abdul Hannan <abdul.hannan@propsure.com.pk>
2023-06-09 16:02:57 +0500

    Integrated Stripe Checkout and made minor improvements, and All Products

--- src/app/pages/shop/kids/page.tsx
+++ src/app/pages/shop/kids/page.tsx
@@ -5,7 +5,7 @@ function Page() {
   return (
       <div className={'flex flex-col gap-y-7 justify-center items-center h-screen'}>
         <HiOutlineEmojiSad size={50} color={'#6f7784'}/>
-        <h1 className={'text-3xl text-gray-500'}>
+        <h1 className={'text-3xl text-gray-500 px-5 text-center'}>
           Sorry! No products to show
         </h1>
     </div>
