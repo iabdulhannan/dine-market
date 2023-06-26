54d356501e4b2c098e8aa1e9a50330e002807651
Abdul Hannan <abdul.hannan@propsure.com.pk>
2023-06-26 17:30:40 +0500

    Integrated Clerk and Neon Postgres

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,6 @@
 import {authMiddleware, clerkClient} from "@clerk/nextjs";
-import {User, users} from "../drizzle/schemas/usersSchema";
-import {connectToDatabase, databaseConnection} from "@/app/databaseConnection";
-import postgres from "postgres";
-import {migrate} from "drizzle-orm/postgres-js/migrator";
-import {drizzle} from "drizzle-orm/postgres-js";
-import toast from "react-hot-toast";
 export default authMiddleware({
   publicRoutes:['/','/pages/shop(.*)', '/pages/auth/signin'],
-  // afterAuth: async (auth, req, evt) => {
-  //   if (auth.userId) {
-  //     const user = await clerkClient.users.getUser(auth.userId);
-  //     // console.log("Auth", auth)
-  //     console.log("Auth", user)
-  //     toast.error('Could not create user', {position: 'top-center'})
-  //     //   Signing in
-  //     // const conn  = await connectToDatabase();
-  //     // console.log('Conn: ', conn)
-  //     // const result: User[] = await conn?.select().from(users);
-  //     // console.log("Result", result)
-  //   }
-  // },
-
-
 });
 
 export const config = {
